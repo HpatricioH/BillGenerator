07e66f64d199206c5e2a0715b61e064c41a60441
HPHuerta <hhernanp@gmail.com>
2023-10-18 23:02:50 -0400

    feat: header model UI fixed. Button for log out added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-4 pb-24">
+    <main className="flex min-h-screen flex-col items-center justify-between px-4 pb-24 bg-[#030712] text-[#fff]">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1 className="left-0 top-0 flex w-full justify-center pb-6 pt-4 dark:border-neutral-800">Bill Generator</h1>
       </div>
