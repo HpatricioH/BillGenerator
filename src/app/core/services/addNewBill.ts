e6aefbd95f58eb6892eed90ef087eaea534ab845
HPHuerta <hhernanp@gmail.com>
2023-11-08 22:07:05 -0500

    Update API URL and format code

--- src/app/core/services/addNewBill.ts
+++ src/app/core/services/addNewBill.ts
@@ -1,19 +1,31 @@
 import { NewBillProps } from "@/app/lib/types/addNewBill";
 
 
-const addNewBill = async ({ address, city, province, postalCode, phone, billTo, description, quantity, UnitPrice, amount, userId }: NewBillProps) => {
-  const URL = 'http://localhost:3000/api/bill'
+const addNewBill = async (
+  {
+    address,
+    city,
+    province,
+    postalCode,
+    phone,
+    billTo,
+    description,
+    quantity,
+    UnitPrice,
+    amount,
+    userId }: NewBillProps) => {
+  const URL = 'https://bill-generator-kappa.vercel.app/api/bill'
 
   const numQuantity = Number(quantity)
   const numUnitPrice = Number(UnitPrice)
   const numAmount = Number(amount)
 
   const response = await fetch(URL, {
-    method: 'POST', 
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ address, city, province, postalCode, phone, billTo, description, quantity:numQuantity, UnitPrice:numUnitPrice, amount:numAmount, userId })
+    body: JSON.stringify({ address, city, province, postalCode, phone, billTo, description, quantity: numQuantity, UnitPrice: numUnitPrice, amount: numAmount, userId })
   })
 
   return await response.json()
