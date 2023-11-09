8fd0ebc097ebdeaf6faba0f3f2b89f892efa7824
HPHuerta <hhernanp@gmail.com>
2023-11-08 22:50:21 -0500

    Commented out EmailProvider in authOptions.ts

--- src/auth/authOptions.ts
+++ src/auth/authOptions.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import { NextAuthOptions } from "next-auth"
 import { PrismaAdapter} from "@next-auth/prisma-adapter"
 import prisma from "@/app/lib/db/db"
-import EmailProvider from "next-auth/providers/email"
+// import EmailProvider from "next-auth/providers/email"
 import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions: NextAuthOptions = {
@@ -16,17 +16,17 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST!,
-        port: process.env.EMAIL_SERVER_PORT!,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER!,
-          pass: process.env.EMAIL_SERVER_PASSWORD!,
-        },
-      },
-      from: process.env.EMAIL_FROM!,
-    })
+    // EmailProvider({
+    //   server: {
+    //     host: process.env.EMAIL_SERVER_HOST!,
+    //     port: process.env.EMAIL_SERVER_PORT!,
+    //     auth: {
+    //       user: process.env.EMAIL_SERVER_USER!,
+    //       pass: process.env.EMAIL_SERVER_PASSWORD!,
+    //     },
+    //   },
+    //   from: process.env.EMAIL_FROM!,
+    // })
   ],
 
   callbacks: {
