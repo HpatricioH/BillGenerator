5e762d0d08dbd96cd99ac3484cb1cf3d5c3f0c0b
HPHuerta <hhernanp@gmail.com>
2024-05-06 23:59:11 -0400

    add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ export default async function Home() {
   return (
     <main className="hero h-[calc(100vh-10.3rem)] bg-dark-primary text-[#fff] relative">
       <div className="hero-content text-center absolute md:top-40 lg:top-44 xl:top-52">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">
+        <div className="background-gradient">
+        </div>
+        <div className="max-w-md z-10">
+          <h1 className="text-5xl font-bold ">
             Bill<span className="text-dark-secondary uppercase"> Generator</span>
           </h1>
-          <div className="flex flex-col items-center gap-2">
+          <div className="flex flex-col items-center gap-2 z-10">
             <p className="py-6">
               {getBill.greeting}
             </p>
