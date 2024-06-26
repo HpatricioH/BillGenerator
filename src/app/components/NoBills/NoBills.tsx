48164e955858067d1917fb447b91965126cf35bc
HPHuerta <hhernanp@gmail.com>
2024-06-25 23:40:00 -0400

    feat: Add AutoCardContainer component for displaying auto-bills

--- src/app/components/NoBills/NoBills.tsx
+++ src/app/components/NoBills/NoBills.tsx
@@ -2,16 +2,16 @@ import PlusSvg from "@/app/core/ui/svgs/PlusSvg";
 import SparklesSvg from "@/app/core/ui/svgs/SparklesSvg";
 import { Button } from "@/app/core/utils/Button";
 
-export default function NoBills() {
+export default function NoBills({ type }: { type: string }) {
   return (
     <main className="hero h-[calc(100vh-238px)] relative">
       <div className="text-center absolute flex flex-col justify-center items-center gap-2 top-40">
         <SparklesSvg className="fill-white w-16 h-16" />
-        <h1 className="text-3xl font-bold">No bills found</h1>
+        <h1 className="text-3xl font-bold">No {type} found</h1>
         <Button className="btn-sm">
           <div className="flex justify-center items-center gap-1">
             <PlusSvg className="w-5 h-5 fill-white" />
-            Create a new Bill
+            Create a new {type}
           </div>
         </Button>
       </div>
--- src/app/dashboard/Auto-generate/page.tsx
+++ src/app/dashboard/Auto-generate/page.tsx
@@ -1,8 +1,25 @@
-export default function Page() {
+import AutoCardContainer from "@/app/components/AutoCardContainer/AutoCardContainer";
+import { getServerAuthSession } from "@/server/auth";
+import { redirect } from "next/navigation"
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: {
+    query: string;
+  }
+}) {
+  const session = await getServerAuthSession()
+  const query = searchParams?.query ?? ''
+
+  if (!session) [
+    redirect('/login')
+  ]
+
   return (
     <main className="hero h-[calc(100vh-238px)] relative">
       <div className="flex text-center absolute top-5 w-full">
-        <h1>Auto-Generate Bills</h1>
+        <AutoCardContainer query={query} />
       </div>
     </main>
   )
