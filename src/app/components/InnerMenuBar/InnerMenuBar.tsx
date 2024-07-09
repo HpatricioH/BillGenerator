e39a664a5916469e4c57b0e99133d38d500d73fd
HPHuerta <hhernanp@gmail.com>
2024-07-09 00:38:54 -0400

    feat: Improve InnerMenuBar navigation styling and functionality

--- src/app/components/InnerMenuBar/InnerMenuBar.tsx
+++ src/app/components/InnerMenuBar/InnerMenuBar.tsx
@@ -1,14 +1,27 @@
+'use client'
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function InnerMenuBar() {
+  const pathname = usePathname()
+
   return (
     <div className="border-b-2 border-white/10 bg-dark-primary navbar pb-0">
       <ul className="justify-start gap-6">
         <li>
-          <Link href={'/dashboard'}>Bills</Link>
+          <Link
+            href={'/dashboard'}
+            className={pathname === '/dashboard' ? 'text-dark-secondary tracking-wide font-bold' : 'text-white tracking-wide'}>
+            Bills
+          </Link>
         </li>
         <li>
-          <Link href={'/dashboard/Auto-generate'}>Auto-Generate Bills</Link>
+          <Link
+            href={'/dashboard/Auto-generate'}
+            className={pathname !== '/dashboard' ? 'text-dark-secondary tracking-wide font-bold' : 'text-white tracking-wide'}>
+            Auto-Generate Bills
+          </Link>
         </li>
       </ul>
     </div>
